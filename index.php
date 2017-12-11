73ba02ed37e53cbde25c7ccb626bd672b1856e78
reysabogal <33353838+reysabogal@users.noreply.github.com>
2017-12-10 21:53:46 -0500

    Add files via upload
    cambios en el index.php

--- index.php
+++ index.php
@@ -1,74 +1,46 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<link rel="stylesheet" href="css/style.css" type="text/css"> 
-
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Programando en PHP</title>
-
-<link rel="shortcut icon" href="images/prog.ico">
-<style>
+  <link rel="stylesheet" href="css/style.css" type="text/css" />
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Programando en PHP</title>
+  <link rel="shortcut icon" href="images/prog.ico" />
+  <style>
 /*
 @import url('https://fonts.googleapis.com/css?family=Chewy');
 */
 @import url('https://fonts.googleapis.com/css?family=Lato:400,700');
-</style>
-
+  </style><?php include("gtag.html") ?>
+//tag google?&gt;
 </head>
-
 <body>
-<?php  include ("mem_sup.html");  //cara principal?>
-
+<?php include ("mem_sup.html");  //cara principal?>
 <div id="inicial1">
-<div id="nota1"> 
-<?php include ("mem_art.html"); ?>
-<!--
-  <br>
-  <div id="prin_art">
-  <div id="art1"><img src="images/img_prue.jpg" width="100" height="100"/></div><div id="art2"><a>primer articulo</a></div>
-  </div>
-  <br>
-  <div id="prin_art">
-  <div id="art1"><img src="images/img_prue.jpg" width="100" height="100"/></div><div id="art2"><a>Segundo articulo</a></div>
-  </div>
-  <br>
-  <div id="prin_art">
-  <div id="art1"><img src="images/img_prue.jpg" width="100" height="100"/></div><div id="art2"><a>Tercer Articulo</a></div>
-  </div>
-  -->
-</div>
-
+<div id="nota1"><?php include ("mem_art.html"); ?></div>
 <div id="nota2">
-  <h2>Bienvenidos al portal Web Programando en PHP</h2>
-  <p>&nbsp;</p>
-  <img src="images/php.jpg" style="float:right; padding: 10px;" width="300" height="200" />
-  <p dir="ltr"><strong id="docs-internal-guid-cd063852-56a6-980c-1db3-991040cfebe1">Es un honor compartir contigo la información que se encuentra contenída en los diferentes artículos y esperamos que sea de tu agrado y que los artículos encontrados puedan ayudarse a solucionar algún problema que tengas o simplemente puedas conocer algo más acerca de algún tema en particular.</strong></p>
-  <p dir="ltr">&nbsp;</p>
-  <p dir="ltr"><strong>Este sitio se encuentra diseñado especialmente para temas de programación en PHP y algunos otros temas que se relacionan en cierta medida con la programación Web.</strong></p>
-  <p dir="ltr">&nbsp;</p>
-  <p dir="ltr"><strong>Esperamos que este sitio sea de tu agrado.</strong></p>
-  <p dir="ltr">&nbsp;</p>
-  <p dir="ltr">&nbsp;</p>
-  <p dir="ltr">&nbsp;</p>
-  <p dir="ltr">&nbsp;</p>
-  <p dir="ltr">&nbsp;</p>
-  <p dir="ltr">&nbsp;</p>
-  <p dir="ltr">&nbsp;</p>
-  <p dir="ltr">&nbsp;</p>
-  <p dir="ltr">&nbsp;</p>
-  <p dir="ltr">&nbsp;</p>
+<h2>Bienvenidos al portal Web Programando en PHP</h2>
 <p>&nbsp;</p>
-  <p>&nbsp;</p>
-  <p>&nbsp;</p>
-  <p>&nbsp;</p>
-  <p>&nbsp;</p>
-  <p>&nbsp;</p>
-  <p>&nbsp;</p>
-  <p>&nbsp;</p>
+<img src="images/php.jpg" style="padding: 10px; float: right;"
+ height="200" width="300" />
+<p><strong>Es un
+honor compartir contigo la información que se encuentra contenída en
+los diferentes artículos y esperamos que sea de tu agrado y que los
+artículos encontrados puedan ayudarse a solucionar algún problema que
+tengas o simplemente puedas conocer algo más acerca de algún tema en
+particular.</strong></p>
+<p>&nbsp;</p>
+<p><strong>Este sitio se encuentra diseñado especialmente
+para temas de programación en PHP y algunos otros temas que se
+relacionan en cierta medida con la programación Web.</strong></p>
+<p>&nbsp;</p>
+<p><strong>Esperamos que este sitio sea de tu agrado.</strong></p>
+<p><br />
+</p>
 </div>
 </div>
-<div id="footer2"><center>
-Programando en PHP 2017
-</center></div>
+<div id="footer">
+<center><strong><strong>Programando en PHP 2017
+</strong></strong></center>
+</div>
 </body>
 </html>
